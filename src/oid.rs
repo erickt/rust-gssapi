5026cd5e0e764b41983f0e23076e8e6db645a409
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2016-07-15 09:52:12 -0700

    Only deallocate OIDs if they're not static

--- src/oid.rs
+++ src/oid.rs
@@ -5,12 +5,14 @@ use error::Error;
 #[derive(Clone, Debug)]
 pub struct OID {
     oid: gssapi_sys::gss_OID,
+    owned: bool,
 }
 
 impl OID {
     pub unsafe fn new(oid: gssapi_sys::gss_OID) -> Self {
         OID {
             oid: oid,
+            owned: true,
         }
     }
 
@@ -19,11 +21,17 @@ impl OID {
     }
 
     pub fn nt_hostbased_service() -> Self {
-        unsafe { OID::new(gssapi_sys::GSS_C_NT_HOSTBASED_SERVICE) }
+        OID {
+            oid: gssapi_sys::GSS_C_NT_HOSTBASED_SERVICE,
+            owned: false,
+        }
     }
     
     pub fn nt_user_name() -> Self {
-        unsafe { OID::new(gssapi_sys::GSS_C_NT_USER_NAME)}
+        OID {
+            oid: gssapi_sys::GSS_C_NT_USER_NAME,
+            owned: false,
+        }
     }
 
     pub unsafe fn get_handle(&self) -> gssapi_sys::gss_OID {
@@ -33,16 +41,18 @@ impl OID {
 
 impl Drop for OID {
     fn drop(&mut self) {
-        let mut minor_status = 0;
-        let major_status = unsafe {
-            gssapi_sys::gss_release_oid(
-                &mut minor_status,
-                &mut self.oid)
-        };
+        if self.owned {
+            let mut minor_status = 0;
+            let major_status = unsafe {
+                gssapi_sys::gss_release_oid(
+                    &mut minor_status,
+                    &mut self.oid)
+            };
 
-        if major_status != gssapi_sys::GSS_S_COMPLETE {
-            let err = Error::new(major_status, minor_status, OID::empty());
-            panic!("{}", err);
+            if major_status != gssapi_sys::GSS_S_COMPLETE {
+                let err = Error::new(major_status, minor_status, OID::empty());
+                panic!("{}", err);
+            }
         }
     }
 }
